d3c375af9bfff806a3fc67a7f85726abbdd7ec8e
Karan Singh <karan.s1606@gmail.com>
2024-01-05 23:37:34 +0530

    stack test case fail

--- src/day1/Stack.ts
+++ src/day1/Stack.ts
@@ -1,18 +1,25 @@
 export default class Stack<T> {
     public length: number;
-
-    
+    data: T[];
 
     constructor() {
+        this.data = [];
+        this.length = 0;
     }
 
     push(item: T): void {
-
-}
+        this.length += 1;
+        this.data.push(item);
+    }
     pop(): T | undefined {
-
-}
+        if (this.length > 0) {
+            this.length -= 1;
+            return this.data.pop();
+        } else {
+            return undefined;
+        }
+    }
     peek(): T | undefined {
-
-}
+        return this.data[this.length];
+    }
 }
