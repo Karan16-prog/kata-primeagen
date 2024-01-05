f12eec197a5000a065b1262e21015e60719020a5
Karan Singh <karan.s1606@gmail.com>
2024-01-06 01:14:59 +0530

    stack via linked list

--- src/day1/Stack.ts
+++ src/day1/Stack.ts
@@ -1,24 +1,39 @@
+type Node<T> = {
+    val: T;
+    prev?: Node<T>;
+};
 export default class Stack<T> {
     public length: number;
-    data: T[];
-
+    private head?: Node<T>;
     constructor() {
-        this.data = [];
         this.length = 0;
+        this.head = undefined;
     }
 
     push(item: T): void {
-        this.length += 1;
-        this.data.push(item);
-    }
-    pop(): T | undefined {
-        let item = this.data.pop();
-        if (this.length != 0) {
-            this.length = this.length - 1;
+        let node = { val: item } as Node<T>;
+        this.length++;
+        if (!this.head) {
+            this.head = node;
+            return;
         }
-        return item;
+        node.prev = this.head;
+        this.head = node;
+        return;
+    }
+
+    pop(): T | undefined {
+        this.length = Math.max(0, this.length - 1);
+        if (this.length === 0) {
+            const head = this.head;
+            this.head = undefined;
+            return head?.val;
+        }
+        let temp = this.head as Node<T>;
+        this.head = temp.prev;
+        return temp?.val;
     }
     peek(): T | undefined {
-        return this.data[this.data.length - 1];
+        return this.head?.val;
     }
 }
